cb6c1fa47ae9400d68af1060caddeb00a0ac3349
Waruggy <48106241+Waruggy@users.noreply.github.com>
2019-09-09 10:32:59 -0700

    Delete Players.test.js

--- client/src/components/Players.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import Players from "./Players";
-
-describe("<Players />", () => {
-  it("renders without crashing", () => {
-    render(<Players />);
-  });
-});
